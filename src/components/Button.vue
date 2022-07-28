cf656abb384429dd31bf2c3a7cb87d0fbe4a0210
asileshi <abebesileshi828@gmail.com>
2022-07-28 04:56:06 -0600

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+<button @click = "temp" :style = "{background:color}" 
+class = "btn">{{text}}</button>
+</template>
+<script>
+export default{
+    name :"Button",
+    props:{
+        text:String,
+        color:String,
+    },
+    methods:{
+        temp(){
+            console.log('the task was added sucessfully')
+        }
+    }
+  
+}
+</script>
---
+++ src/components/task.vue
@@ -0,0 +1,13 @@
+<template>
+<dev :key = "t.id" v-for = "t in tasks">
+
+</dev>
+</template>
+<script>
+export default{
+    mane:"Tasks",
+    props:{
+        tasks:array
+    }
+}
+</script>
